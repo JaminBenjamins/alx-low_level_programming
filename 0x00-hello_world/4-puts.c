5dd77c6bead838e8793930a6b7b8e2efe77661e8
JaminBenjamins <jaminbenjamins@gmail.com>
2022-09-08 17:14:21 -0700

    Basics in C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * THis is a programm that prints a statement on the buffer. 
+ * Returns zero if executed properly or 1 if otherwise. 
+ * */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
