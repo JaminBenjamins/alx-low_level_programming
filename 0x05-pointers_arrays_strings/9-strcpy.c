fec04221dc613e4001c66c46d2d082020106b1f2
JaminBenjamins <jaminbenjamins@gmail.com>
2022-09-20 13:21:37 -0700

    Still doing hard things

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,11 +7,11 @@
  * Return : value pointer to dest
  *
  */
-char *_strcpy(char *dest, char *src);
+char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	while (*(src + i) != '\0\')
+	while (*(src + i) != '\0')
 	{
 		*(dest + i) = *(src + i)
 			i++;
