79997d2a362dae01275912d27e308b0dcf0234be
JaminBenjamins <jaminbenjamins@gmail.com>
2022-09-20 11:41:25 -0700

    Still doing hard things

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * Main : Prints a string in reverse order
@@ -10,6 +11,6 @@ void print_rev(char *s)
 	int len = strlen(*s);
 
 	while (len --)
-		_putchar(*s + len);
+		putchar(*s + len);
 
 }
