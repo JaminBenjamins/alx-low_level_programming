741f69283298b7db6460a534e38c2e3cb303fd30
JaminBenjamins <jaminbenjamins@gmail.com>
2022-09-20 02:35:27 -0700

    Pushin p

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
-/** This is a programm that prints alphabet in lowercase
- * The program runs in the main function
- * */
+/** 
+ * Main : This is a programm that prints alphabet in lowercase
+ * Return : Always 0
+ */ 
 
 int main(void)
 {
 	int a;
-	for (a = 'a'; a < 'z'; a++)
+	for (a = 'a'; a <= 'z'; a++)
 		putchar(a);
 	putchar ('\n');
 	return (0);
