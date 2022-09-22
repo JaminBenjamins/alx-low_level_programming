b720aab86b0e2062bcc698730d3cadfecab6e9f0
JaminBenjamins <jaminbenjamins@gmail.com>
2022-09-22 11:28:33 -0700

    Still counting

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * pirnt all integers from input to 98
+ * separated by a comma and space
+ * @n the first value of the range
+ */
+void print_to_98(int n)
+{
+	if (n >=98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n--);
+		printf("%d\n", n);
+		}
+		else
+			while (n < 98)
+			{
+				printf("%d, ", n++)
+			printf("%d\n", n);
+			}
+	}
+}
