e93fe47ae118ec3b31a0e1e58cf042d98b1f2343
JaminBenjamins <jaminbenjamins@gmail.com>
2022-09-22 17:29:48 -0700

    Pointing to a ray of life

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * string_toupper a function that conversta an array of lowercase to uppercaase
+ * @str string to be changed
+ * Return pointer to the string
+ */
+char *string_toupper(char *)
+{
+	int i = 0;
+
+	while str[i] != '\0';
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str -= 32;
+		i++;
+	}
+	return (str);
+}
