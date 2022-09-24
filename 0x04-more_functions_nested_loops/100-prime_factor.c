b7bd2e7b4bbcabf34e59ccba8e7a08682e583115
JaminBenjamins <jaminbenjamins@gmail.com>
2022-09-24 04:51:01 -0700

    Do it a do

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * main - prints the largest prime factor
+ * of a digit
+ * Return : returns 0
+ */
+int main(void)
+{
+	long num = 612852475143;
+	int inc; 
+
+	while (inc++ < num / 2)
+	{
+	if (num % inc == 0)
+	{
+		num /= 2;
+		continue;
+	}
+	for (inc = 3; inc < num / 2; inc +=2)
+	{
+		if (num % inc == 0)
+			num /= inc;
+	}
+	}
+	printf("%ld\n", num);
+	return (0);
+}
