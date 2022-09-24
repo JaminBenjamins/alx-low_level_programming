d35a185df5b30348bd3d590aea729e9901d0d1e4
JaminBenjamins <jaminbenjamins@gmail.com>
2022-09-24 04:00:58 -0700

    Still doing

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,17 +8,21 @@ int main(void)
 {
 	int n; 
 
-	for (n = 0; n < 100; n++)
+	for (n = 0; n <= 100; n++)
 	{
 		if (n % 3 == 0)
+		{
 			printf("Fizz");
+		}
 		else if (n % 5 == 0)
+		{
 			printf("Buzz");
+			
 		else if ((n % 3 == 0) && (n % 5 == 0))
 			printf("Fizz Buzz");
 		else
 			printf("%d", n);
-		if (n == 100)
+		if (n < 100)
 			continue;
 		printf(" ");
 	}
