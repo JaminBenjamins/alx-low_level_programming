52480d3caac3e585a45ed77971d18c246ff1991e
JaminBenjamins <jaminbenjamins@gmail.com>
2022-09-24 03:12:07 -0700

    Still doing

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
  * Main : The function where everything executes
  * Description : A function that prints a sequence of characters
@@ -14,15 +16,15 @@ void print_diagonal(int n)
 		{
 			for (dash = 0; dash < line; dash++)
 			{
-				_putchar(' ');
+				putchar(' ');
 			}
-		_putchar("\\");
+		putchar("\\");
 		
 			if (line == n - 1)
 			{
 				continue;
 			}
-	_putchar('\n');
+	putchar('\n');
 		}
 	}
 }
