22f286fc2ba7f77ddda6f9830c7ee5bdf1323bfd
JaminBenjamins <jaminbenjamins@gmail.com>
2023-01-04 13:48:22 -0800

    Doing hard things

--- 0x07-pointers_arrays_strings/8-print_diagsum.c
+++ 0x07-pointers_arrays_strings/8-print_diagsum.c
@@ -8,18 +8,14 @@
  */
 void print_diagsum(int *a, int size)
 {
-	int i, j, k, sum1, sum2;
+	int i;
+	unsigned int sum0, sum1;
 
-	k = 0; sum1 = 0; sum2 = 0;
-
-	for (i = 0; i < size; i++)
+	for (i =0; i < size; i++)
 	{
-		for (j = 0; j < size; j++)
-		{
-			(i == j) ? sum1 += a[k] : 0;
-			(i + j == size - 1) ? sum2 += a[k] : 0;
-			k++;
-		}
-	printf("%i,%i\n", sum1, sum2);
+		sum0 += a[(size * i + 1];
+		sum1 += a[(size * (i + 1)) - (i + 1)];
+	}
+	printf("%i,%i\n", sum0, sum1);
 
 }
