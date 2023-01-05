18eab27f04c0b9688029399ac55a7b22e6e75fa3
JaminBenjamins <jaminbenjamins@gmail.com>
2023-01-05 07:03:43 -0800

    Recursions

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,24 +7,14 @@
  */
 int is_prime_number(int n)
 {
-	int emp = n / 2;
-
-	if (n <= 1)
+	int emp = 2;
+	int i;
+	
+	if (n % emp == 0)
 		return(0);
-	return(is_prime(n, emp));
-}
-/**
- * is_prime: returns 1 if n is prime
- * @n: number to be analyzed
- * @emp: number to start checking from
- *
- * Return 1 if n is prime 0 otherwise
- */
-int is_prime(int n, int emp)
-{
-	if (emp <= 1)
+	if (n <= 2)
 		return(1);
-	else if (n % emp == 0)
-		return(0);
-	return(is_prime(n, emp - 1));
+	if (i * i > n)
+		return(1)
+	return(is_prime(n, i + 1));
 }
