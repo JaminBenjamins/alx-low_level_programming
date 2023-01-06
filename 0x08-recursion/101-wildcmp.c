a5acecaf622ea69e8e5d086ef4bce5a67451abe0
JaminBenjamins <jaminbenjamins@gmail.com>
2023-01-06 09:49:46 -0800

    string comparison

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -11,8 +11,8 @@ int wildcmp(char *s1, char *s2)
 	if (*s2 == '\0')
 		return(*s1 == '\0');
 	if (*s2 == *s1)
-		return(*s1 != '\0' && wildcmp(s2 + 1),(s1 + 1));
+		return(*s1 != '\0' && wildcmp(s2 + 1, s1 + 1);
 	if (*s2 == '*')
-		return(wildcmp(s1, s2 + 1) || *s1 == '\0' && wildcmp(s2, s + 1));
+		return(wildcmp(s1, s2 + 1) || (*s1 == '\0' && wildcmp(s2, s + 1)));
 	return(0);
 }
