e3393d658e81448606ba8d205dc4bd4bab44ce9b
JaminBenjamins <jaminbenjamins@gmail.com>
2023-01-30 11:39:22 -0800

    Print list of elements in a struct

---
+++ 0x12-singly_linked_list/0-print_list.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_list - a function that prints a list of characters
+ * @h: pointer to list to be printed
+ *
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t node = 0;
+
+	while(h)
+	{
+		if (h->str != NULL)
+			printf("%d, %s", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
+		node++;
+		h = h->next;
+	}
+	return (node);
+}
