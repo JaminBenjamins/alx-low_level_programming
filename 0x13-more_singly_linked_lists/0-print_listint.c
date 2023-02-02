8eb3008c6bcf88c159e98fef067e8866af513780
JaminBenjamins <jaminbenjamins@gmail.com>
2023-02-02 13:31:42 -0800

    More singly linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,10 +11,10 @@ size_t print_listint(const listint_t *h)
 	const listint_t *p = h; 
 	size_t node;
 
-	while (h != NULL)
+	while (p != NULL)
 	{
 		printf("%d\n", p->n);
-		h = h->next;
+		p = p->next;
 		node++;
 	}
 	return (node);
