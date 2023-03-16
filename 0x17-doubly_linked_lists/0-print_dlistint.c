3d54566056e48d25b6d45cba15f0493320a2c2a7
JaminBenjamins <jaminbenjamins@gmail.com>
2023-03-16 04:50:44 -0700

    print list of elements in a list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,7 +7,7 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	int i = 0;
-	
+
 	if (h == NULL)
 		return (i);
 
@@ -20,5 +20,5 @@ size_t print_dlistint(const dlistint_t *h)
 		h = h->next;
 		i++;
 	}
-	return(i);
+	return (i);
 }
