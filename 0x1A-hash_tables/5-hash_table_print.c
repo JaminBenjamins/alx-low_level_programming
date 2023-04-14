190339638015404634732a75f41f0a8d977eb3fb
JaminBenjamins <jaminbenjamins@gmail.com>
2023-04-14 01:14:19 -0700

    Printing a hash table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -25,7 +25,7 @@ void hash_table_print(const hash_table_t *ht)
 			node = ht->array[index];
 			while (node != NULL)
 			{
-				print("%s, %s", node->key, node->value);
+				printf("%s, %s", node->key, node->value);
 				node = node->next;
 				if (node != NULL)
 					printf(",");
