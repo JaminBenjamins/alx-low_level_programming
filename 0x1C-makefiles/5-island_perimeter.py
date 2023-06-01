a2463e6eb28d568439336092487ce7b34059986d
jaminbenjamins <jaminbenjamins@gmail.com>
2023-06-01 13:58:52 +0300

    Making code pep8 compliant

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,9 +3,9 @@
 
 
 def island_perimeter(grid):
-    """ Return perimeter of an area 
+    """ Return perimeter of an area
         The grid represents water by 0 and 1 land
-        Args: 
+        Args:
             grid(list): A list of values representing an island
         Return:
             The perimeter area defined by grid
@@ -17,9 +17,9 @@ def island_perimeter(grid):
 
     for i in range(height):
         for j in range(width):
-            if grid [i][j] == 1:
+            if grid[i][j] == 1:
                 size += 1
-                if j > 0  and grid[i][j - 1] == 1:
+                if j > 0 and grid[i][j - 1] == 1:
                     edges += 1
                 if i > 0 and grid[i - 1][j] == 1:
                     edges += 1
