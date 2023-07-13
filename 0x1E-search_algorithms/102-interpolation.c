17c9e9651b6cf0e958c4eb941ef2e5459266e67d
JaminBenjamins <jaminbenjamins@gmail.com>
2023-07-13 05:38:35 +0300

    Implementing interpolation search

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- * Interpolation_search - function that searches
+ * interpolation_search - function that searches
  * for a value in a sorted array of integers
  * @array: pointer to the first element of the array to search
  * @size: number of elements in array
@@ -11,27 +11,27 @@
  */
 int interpolation_search(int *array, size_t size, int value)
 {
-    size_t i, l, r;
+	size_t i, l, r;
 
-    if (array == NULL)
-        return (-1);
+	if (array == NULL)
+		return (-1);
 
-    for (l = 0, r = size - 1; r >= l;)
-    {
-        i = l + (((double)(r - l) / array[r] - array[l])) * (value - array[l]));
-        if (i < size)
-            printf("Value checke array[%ld] is out of range\n", i);
-        esle
-        {
-            printf("Value checked array[%ld] is out of range\n", i);
-            break;
-        }
-        if (array[i] == value)
-            return (i);
-        if (array[i] > value)
-            r = i - 1;
-        else
-            l = i + 1;
-    }
-    return (-1);
+	for (l = 0, r = size - 1; r >= l;)
+	{
+		i = l + (((double)(r - l) / array[r] - array[l])) * (value - array[l]));
+		if (i < size)
+			printf("Value checke array[%ld] is out of range\n", i);
+		esle
+		{
+			printf("Value checked array[%ld] is out of range\n", i);
+			break;
+		}
+		if (array[i] == value)
+			return (i);
+		if (array[i] > value)
+			r = i - 1;
+		else
+			l = i + 1;
+	}
+	return (-1);
 }
