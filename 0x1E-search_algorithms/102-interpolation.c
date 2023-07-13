f8d2f1009995af5e6aa810e24499c29222565702
JaminBenjamins <jaminbenjamins@gmail.com>
2023-07-13 18:13:32 +0300

    Inserting the right brackets

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -18,10 +18,10 @@ int interpolation_search(int *array, size_t size, int value)
 
 	for (l = 0, r = size - 1; r >= l;)
 	{
-		i = l + (((double)(r - l) / array[r] - array[l])) * (value - array[l]));
+		i = l + (((double)(r - l) / (array[r] - array[l])) * (value - array[l]));
 		if (i < size)
 			printf("Value checke array[%ld] is out of range\n", i);
-		esle
+		else
 		{
 			printf("Value checked array[%ld] is out of range\n", i);
 			break;
