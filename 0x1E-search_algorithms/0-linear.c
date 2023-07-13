e58278135bb0249b8227e4948132edec1bb27565
JaminBenjamins <jaminbenjamins@gmail.com>
2023-07-13 05:37:02 +0300

    Perform a linear search in an unsorted array

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,16 +10,16 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-    int i;
+	int i;
 
-    if (array == NULL)
-        return (-1);
-    for (i = 0; i < (int)size; i++)
-    {
-        printf("Value checked array[%d] = [%d]\n", i, *array);
-        if (*array == value)
-            return (i);
-        array++;
-    }
-    return (-1);
+	if (array == NULL)
+		return (-1);
+	for (i = 0; i < (int)size; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, *array);
+		if (*array == value)
+			return (i);
+		array++;
+	}
+	return (-1);
 }
